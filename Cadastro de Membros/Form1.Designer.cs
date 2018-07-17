acc7ec31f7e46920630077c346104552f50f7877
Luana Caroline <luannanenis@gmail.com>
2018-07-17 17:24:57 -0300

    ajustes

--- Cadastro de Membros/Form1.Designer.cs
+++ Cadastro de Membros/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.Consulta = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.consultaMembroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.Consulta.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,14 +46,24 @@
             // consultaMembroToolStripMenuItem
             // 
             this.consultaMembroToolStripMenuItem.Name = "consultaMembroToolStripMenuItem";
-            this.consultaMembroToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.consultaMembroToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
             this.consultaMembroToolStripMenuItem.Text = "Consulta Membro";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(602, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -64,6 +75,7 @@
         #endregion
         private System.Windows.Forms.ContextMenuStrip Consulta;
         private System.Windows.Forms.ToolStripMenuItem consultaMembroToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
